1c3273cae663a3a91cf5418d56ecec695e9904e1
Antonio Santana <asantana35@usf.edu>
2024-05-21 16:28:05 -0400

    cc_2.js
    "Add brake method to decrease speed by 5 km/h."

--- cc_2.js
+++ cc_2.js
@@ -12,5 +12,11 @@ accelerate() {
     this.speed += 10;
     console.log(`${this.make} is going at ${this.speed} km/h`);
     }
+    
+// Brake Method
+brake() {
+    this.speed -= 5;
+    console.log(`${this.make} is going at ${this.speed} km/h`);
+   }
 
 }
