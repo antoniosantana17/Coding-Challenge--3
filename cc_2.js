720f883340d62caa13074614012086979ea47295
Antonio Santana <asantana35@usf.edu>
2024-05-21 16:30:42 -0400

    cc_2.js
    "Test accelerate and brake methods on Car instances."

--- cc_2.js
+++ cc_2.js
@@ -19,4 +19,18 @@ brake() {
     console.log(`${this.make} is going at ${this.speed} km/h`);
    }
 
+// Create and Test Car Objects
+const car1 = new Car('BMW', 120);
+const car2 = new Car('Mercedes', 95);
+
+car1.accelerate(); // BMW is going at 130 km/h
+car1.brake();     // BMW is going at 125 km/h
+car1.accelerate(); // BMW is going at 135 km/h
+car1.brake(); // BMW is going at 130 km/h
+
+car2.accelerate(); // Mercedes is going at 105 km/h
+car2.brake(); // Mercedes is going at 100 km/h
+car2.accelerate(); // Mercedes is going at 110 km/h
+car2.brake(); // Mercedes is going at 105 km/h
+   
 }
